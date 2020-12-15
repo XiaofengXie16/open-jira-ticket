67cc1cd7bb71efeff6d5104dc41479396e7ef6f8
Xiaofeng Xie <Xiaofeng.Xie@libertymutual.com>
2020-12-15 00:30:50 -0500

    feat: added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-  extends: ["airbnb", "plugin:prettier/recommended", "prettier/react"],
-  plugins: ["jest", "jsx-a11y", "prettier", "react-hooks", "react"],
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  plugins: ['jest', 'jsx-a11y', 'prettier', 'react-hooks', 'react'],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
   env: {
     node: true,
+    jest: true,
   },
   rules: {
-    "react/jsx-filename-extension": [0],
-    "no-multi-spaces": ["error"],
+    'react/jsx-filename-extension': [0],
+    'no-multi-spaces': ['error'],
+    // Prettier Rules
+    'prettier/prettier': ['error', { arrowParens: 'avoid', printWidth: 120, singleQuote: true, trailingComma: 'es5' }],
   },
 };
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,22 @@
-import { execSync } from "child_process";
-import BASE_URL from "./constants";
+import { execSync } from 'child_process';
+import BASE_URL from './constants';
 
-const executeGitCommand = (command) =>
+const executeGitCommand = command =>
   execSync(command)
-    .toString("utf8")
-    .replace(/[\n\r\s]+$/, "");
+    .toString('utf8')
+    .replace(/[\n\r\s]+$/, '');
 
-const getCurrentBranchName = () =>
-  executeGitCommand("git rev-parse --abbrev-ref HEAD");
+const getCurrentBranchName = () => executeGitCommand('git rev-parse --abbrev-ref HEAD');
 
 const getTicketNumber = () => {
   const branch = getCurrentBranchName();
   const number = branch.match(/[a-zA-Z]+-[0-9]+/i);
-  return number?.[0] || "";
+  return number?.[0] || '';
 };
 
 export const setBasePath = (config, basePath) => {
-  const baseUrl = basePath.endsWith("/") ? basePath : `${basePath}/`;
+  const baseUrl = basePath.endsWith('/') ? basePath : `${basePath}/`;
   config.set(BASE_URL, baseUrl);
 };
 
-export const getFullUrl = (baseUrl) => `${baseUrl}${getTicketNumber()}`;
+export const getFullUrl = baseUrl => `${baseUrl}${getTicketNumber()}`;
